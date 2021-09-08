fb351ed0018f9b6d741840b4de1b3b024305d124
v_chenchao <v_chenchao@tencent.com>
2021-09-08 11:54:01 +0800

    目录初始化

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="nav">
+      <router-link to="/">
+      <svg-icon icon-class="wx"></svg-icon>
+      <!-- <svg>
+        <use xlink:href="#icon-wx"></use>
+      </svg> -->
+      Home</router-link> |
+      <router-link to="/about">
+      <svg-icon icon-class="qq"></svg-icon>
+      About</router-link>
+    </div>
+    <!-- <p @click="$store.commit('add')">{{$store.state.counter}}</p>
+    <p @click="$store.dispatch('add')">async: {{$store.state.counter}}</p> -->
+    <!-- <p>doubleCounter: {{$store.getters.doubleCounter}}</p> -->
+    <router-view/>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// import '@/icons/svg/wx.svg'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  
 }
 </script>
-
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
